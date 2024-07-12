43ef7bcb4c3fb5e22c9629479f8f6dd0e3417760
mtm-YuzanaKyaw <mtm.yuzanakyaw@gmail.com>
2024-07-12 10:33:10 +0630

    add post model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_11_102759) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_12_040147) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "product_types", force: :cascade do |t|
     t.string "product_type"
     t.datetime "created_at", null: false
@@ -33,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_102759) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
